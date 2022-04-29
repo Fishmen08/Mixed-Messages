7100dadfc1f93a70df7a3fa2b01a228672e56bc3
Fishmen <adam_phoz18@hotmail.com>
2022-04-29 12:25:28 +1000

    created the year randomizer within next 50 years

--- index.js
+++ index.js
@@ -1,2 +1,8 @@
 console.log('Mixed Messages Project');
 console.log(6 * 5);
+
+// making a loop to find a random year just realised i dont need a loop for this
+
+let year = 2022+(Math.floor(Math.random()* (Math.ceil(Math.random() * 50))));
+    
+console.log(year)
