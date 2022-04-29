c0eb463feb810c943dc1790ef0b5996d9e03b258
Fishmen <adam_phoz18@hotmail.com>
2022-04-29 12:31:17 +1000

    we have the random month selector

--- index.js
+++ index.js
@@ -4,5 +4,8 @@ console.log(6 * 5);
 // making a loop to find a random year just realised i dont need a loop for this
 
 let year = 2022+(Math.floor(Math.random()* (Math.ceil(Math.random() * 50))));
-    
+let month = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'Novemeber', 'December'];
+let randomMonth = month[Math.floor(Math.random() * 12)];    
 console.log(year)
+console.log(randomMonth)
+
